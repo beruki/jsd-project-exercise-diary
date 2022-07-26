886a473edbab7ccb55decbc67ad46f415e92e67c
beruki <berukii.z@live.jp>
2022-07-26 16:44:09 +0700

    mui adding

--- src/components/page/App/App.jsx
+++ src/components/page/App/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Button from '@mui/material/Button';
+
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,9 +20,10 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <Button variant="contained" onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </Button>
+
         <p>
           Edit <code>src/App.jsx</code> eiei save to test HMR
         </p>
