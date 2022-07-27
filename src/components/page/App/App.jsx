cc6288bab75c3c8ff51d561a83ddb11c544ea73d
beruki <berukii.z@live.jp>
2022-07-27 16:50:10 +0700

    set button css

--- src/components/page/App/App.jsx
+++ src/components/page/App/App.jsx
@@ -1,28 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Box, createTheme, ThemeProvider } from '@mui/material';
 import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import * as React from 'react';
+import './App.css'
 
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const theme = createTheme({
+    
+  });
 
   return (
-    <div className="App">
-      <div className="button-test">
-        <Button variant="contained" onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-
-        <p>
-          test mui import
-        </p>
-      </div>
-
-    </div>
-  )
+    <ThemeProvider theme={theme}>
+      <Box>
+        {/* SX */}
+      <Stack spacing={2} direction="row">
+        <Button className='Text-Button' variant="text">Text</Button>
+        <Button className='Primary-Button' sx={{width: 300}} variant="contained">Contained</Button>
+        <Button className='Secondary-Button' style={{width: 300}} variant="outlined">Outlined</Button>
+      </Stack>
+      </Box>
+    </ThemeProvider>
+  );
 }
-
-export default App
