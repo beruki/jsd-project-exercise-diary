b1a9dff4068070b2b6fcd642db2cb6f43ac55ba7
beruki <berukii.z@live.jp>
2022-07-29 10:20:26 +0700

    add button css

--- src/components/page/App/App.jsx
+++ src/components/page/App/App.jsx
@@ -1,9 +1,10 @@
 import { Box, createTheme, ThemeProvider } from '@mui/material';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
+import { TextField } from '@mui/material';
 import * as React from 'react';
 import './App.css'
-
+import '../../button/button.css'
 
 export default function App() {
   const theme = createTheme({
@@ -17,8 +18,14 @@ export default function App() {
       <Stack spacing={2} direction="row">
         <Button className='Text-Button' variant="text">Text</Button>
         <Button className='Primary-Button' sx={{width: 300}} variant="contained">Contained</Button>
-        <Button className='Secondary-Button' style={{width: 300}} variant="outlined">Outlined</Button>
+        <Button className='Secondary-Button' sx={{borderColor: 'rgba(178, 31, 31, 1)'}} variant="outlined">Outlined</Button>
       </Stack>
+
+        {/* SX */}
+      <Stack spacing={2} direction="row">
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      </Stack>
+
       </Box>
     </ThemeProvider>
   );
