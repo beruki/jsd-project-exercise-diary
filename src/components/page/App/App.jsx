463ce8105c14e70c9ba73142b94c976affe6c345
beruki <berukii.z@live.jp>
2022-08-01 21:16:25 +0700

    all card custom css

--- src/components/page/App/App.jsx
+++ src/components/page/App/App.jsx
@@ -49,7 +49,7 @@ export default function App() {
   <ThemeProvider theme={theme}>
     <Box>
       <Stack>
-        <Card sx={{ maxWidth: 345 }}>
+        <Card sx={{ maxWidth: 345 }} className='Run-card'>
         <CardHeader
           action={
             <IconButton aria-label="settings">
@@ -82,7 +82,7 @@ export default function App() {
       </Stack>
 
       <Stack>
-        <Card sx={{ maxWidth: 345 }}>
+        <Card sx={{ maxWidth: 345 }} className='Swim-card'>
         <CardHeader
           action={
             <IconButton aria-label="settings">
@@ -90,29 +90,96 @@ export default function App() {
             </IconButton>
           }
           subheader="12 Jun 2022"
-          title="Run for mom"
+          title="Swim at pool party"
         />
 
         <CardMedia
           component="img"
           height="188"
-          image="../../../../public/running-desk.png"
-          alt="running"
+          image="../../../../public/swim-desk.png"
+          alt="swimming"
         />
 
         <CardContent>
           <Typography variant="body2" color="text.secondary">
-          วิ่งที่สวนลุม เหนื่อยมาก วิ่งที่สวนลุม เหนื่อยมาก 
+          ว่ายน้ำที่ปาร์ตี้ เหนื่อยมากๆๆๆ
           </Typography>
           <br />
           <Typography variant="body2" className='Calories'>
           Calories 800 kcal 🔥  
           </Typography>
           <br />
-          <Chip label="Run" color="primary" className='Tag-run' />
+          <Chip label="Swim" color="primary" className='Tag-swim' />
         </CardContent>
         </Card>
       </Stack>
+
+      <Stack>
+        <Card sx={{ maxWidth: 345 }} className='Hike-card'>
+        <CardHeader
+          action={
+            <IconButton aria-label="settings">
+              <MoreVertIcon />
+            </IconButton>
+          }
+          subheader="12 Jun 2022"
+          title="Hike at 12.00"
+        />
+
+        <CardMedia
+          component="img"
+          height="188"
+          image="../../../../public/hiking-desk.png"
+          alt="hiking"
+        />
+
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+          ปีนให้ไกลไปให้ถึงจ้า
+          </Typography>
+          <br />
+          <Typography variant="body2" className='Calories'>
+          Calories 800 kcal 🔥  
+          </Typography>
+          <br />
+          <Chip label="Hike" color="primary" className='Tag-hike' />
+        </CardContent>
+        </Card>
+      </Stack>
+
+      <Stack>
+        <Card sx={{ maxWidth: 345 }} className='Bike-card'>
+        <CardHeader
+          action={
+            <IconButton aria-label="settings">
+              <MoreVertIcon />
+            </IconButton>
+          }
+          subheader="12 Jun 2022"
+          title="Bike for aunt"
+        />
+
+        <CardMedia
+          component="img"
+          height="188"
+          image="../../../../public/ride-desk.png"
+          alt="riding"
+        />
+
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+          ปั่น ปั่นจักรยาน
+          </Typography>
+          <br />
+          <Typography variant="body2" className='Calories'>
+          Calories 800 kcal 🔥  
+          </Typography>
+          <br />
+          <Chip label="Ride" color="primary" className='Tag-ride' />
+        </CardContent>
+        </Card>
+      </Stack>
+
 
     </Box>
   </ThemeProvider>
